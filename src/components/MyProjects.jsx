01914a2e59f97180040164933861400cbf274233
Josh <jmart790@fiu.edu>
2019-09-20 19:31:56 -0400

    added components folder containing 4 hollow components

---
+++ src/components/MyProjects.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MyProjects() {
+  return (
+    <div>
+      <h1>My Projects</h1>
+    </div>
+  )
+}
+
+export default MyProjects
---
+++ src/components/MyStory.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MyStory() {
+  return (
+    <div>
+      <h1>My story</h1>
+    </div>
+  )
+}
+
+export default MyStory
