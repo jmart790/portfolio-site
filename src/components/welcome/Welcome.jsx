a19a9ba9788a4d3169e2de4a56c5a3d7f3ffeede
Josh <jmart790@fiu.edu>
2019-09-21 19:22:14 -0400

    created welcome folder and added dummy data to the welcome component

---
+++ src/components/welcome/Welcome.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Welcome() {
+  return (
+    <div className='welcome-container'>
+      <div className='welcome-text'>
+        <h1>Welcome</h1>
+      </div>
+      <div className='professional-label'>
+        <h4>Software Developer</h4>
+      </div>
+      <div className='professional-label'>
+        <h4>Entrepreneur</h4>
+      </div>
+      <div className='me'>Me</div>
+    </div>
+  )
+}
+
+export default Welcome
