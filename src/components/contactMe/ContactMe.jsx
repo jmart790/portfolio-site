19651a216c0d0e554ca13f0916244771874fd49b
Josh <jmart790@fiu.edu>
2019-09-21 19:23:07 -0400

    created welcome folder and added dummy data to the contact component

---
+++ src/components/contactMe/ContactMe.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function ContactMe() {
+  return (
+    <div className='contact-me-container'>
+      <div>
+        <h1>Contact Me</h1>
+      </div>
+      <div>contact form</div>
+      <div>submit</div>
+      <div>Thank you for stopping bye</div>
+    </div>
+  )
+}
+
+export default ContactMe
