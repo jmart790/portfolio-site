03fc7557f01259aaa8ef888c543e5a1d10bcfdec
Josh <jmart790@fiu.edu>
2019-09-21 19:23:40 -0400

    created projects folder and added dummy data to the projects component

---
+++ src/components/myProjects/MyProjects.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function MyProjects() {
+  return (
+    <div className='my-projects-container'>
+      <div>
+        <h1>My Projects</h1>
+      </div>
+      <div>
+        <div>video</div>
+        <div>title</div>
+        <div>description</div>
+        <div>github link</div>
+        <div>website</div>
+        <div>carousel tracker</div>
+      </div>
+    </div>
+  )
+}
+
+export default MyProjects
