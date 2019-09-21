ae263d320175182398ce7e585671e913d09ebf14
Josh <jmart790@fiu.edu>
2019-09-21 19:24:14 -0400

    created story folder and added dummy data to the story component

---
+++ src/components/myStory/MyStory.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function MyStory() {
+  return (
+    <div className='my-story-container'>
+      <div>
+        <h1>My story</h1>
+      </div>
+      <div>story point 1</div>
+      <div>story point 2</div>
+      <div>story point 3</div>
+      <div>story point etc</div>
+    </div>
+  )
+}
+
+export default MyStory
