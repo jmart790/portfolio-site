49f81741c16fbbaac55731f088273753134698f3
Josh <jmart790@fiu.edu>
2019-09-23 08:06:30 -0400

    changed inner text, added an image and classNames to divs for styling

--- src/components/welcome/Welcome.jsx
+++ src/components/welcome/Welcome.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
+import './welcome.css'
 
 function Welcome() {
   return (
-    <div className='welcome-container'>
+    <div className='welcome-container container'>
       <div className='welcome-text'>
-        <h1>Welcome</h1>
+        <h1>Hi there, I'm Josh</h1>
       </div>
       <div className='professional-label'>
-        <h4>Software Developer</h4>
+        <p>Software Developer</p>
+        <p>Problem Solver</p>
+        <p>Maker of cool things</p>
       </div>
-      <div className='professional-label'>
-        <h4>Entrepreneur</h4>
+      <div className='call-to-action'>v</div>
+
+      <div className='bitmoji'>
+        <img src={require('./wave-pose.jpeg')} alt='wave pose' />
       </div>
-      <div className='me'>Me</div>
     </div>
   )
 }
