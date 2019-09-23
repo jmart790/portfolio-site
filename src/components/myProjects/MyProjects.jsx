ef35249746abac638cd7acf85a6a87bad9c13269
Josh <jmart790@fiu.edu>
2019-09-23 08:04:02 -0400

    added classNames to divs for styling

--- src/components/myProjects/MyProjects.jsx
+++ src/components/myProjects/MyProjects.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
+import './my-projects.css'
 
 function MyProjects() {
   return (
-    <div className='my-projects-container'>
-      <div>
-        <h1>My Projects</h1>
+    <div className='my-projects-container container'>
+      <div className='my-projects'>
+        <h1>
+          My
+          <br />
+          Projects
+        </h1>
       </div>
-      <div>
-        <div>video</div>
-        <div>title</div>
-        <div>description</div>
-        <div>github link</div>
-        <div>website</div>
-        <div>carousel tracker</div>
-      </div>
+
+      <div className='project-video'>video</div>
+      <div className='project-title'>title</div>
+      <div className='project-description'>description</div>
+      <div className='project-links'>Github | Website</div>
+      <div className='carousel-tracker'>carousel tracker</div>
     </div>
   )
 }
