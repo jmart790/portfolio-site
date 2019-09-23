67f0bf92732c6603f7930bb522ae200097482d9a
Josh <jmart790@fiu.edu>
2019-09-23 08:05:10 -0400

    added classNames to divs for styling

--- src/components/myStory/MyStory.jsx
+++ src/components/myStory/MyStory.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
+import './my-story.css'
 
 function MyStory() {
   return (
-    <div className='my-story-container'>
-      <div>
+    <div className='my-story-container container'>
+      <div className='my-story'>
         <h1>My story</h1>
       </div>
-      <div>story point 1</div>
-      <div>story point 2</div>
-      <div>story point 3</div>
-      <div>story point etc</div>
+      <div className='story-one'>Navy Story point</div>
+      <div className='story-two'>College Story point</div>
+      <div className='story-three'>Wyncode Story point</div>
+      <div className='story-four'>Now story point</div>
     </div>
   )
 }
