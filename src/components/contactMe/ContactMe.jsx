6aa1782cc541c4b2bbb6a5fe87d11b3bdbbb6c76
Josh <jmart790@fiu.edu>
2019-09-23 08:02:44 -0400

    added classNames to divs for styling

--- src/components/contactMe/ContactMe.jsx
+++ src/components/contactMe/ContactMe.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
+import './contact-me.css'
 
 function ContactMe() {
   return (
-    <div className='contact-me-container'>
-      <div>
+    <div className='contact-me-container container'>
+      <div className='contact-me'>
         <h1>Contact Me</h1>
       </div>
-      <div>contact form</div>
-      <div>submit</div>
-      <div>Thank you for stopping bye</div>
+      <div className='contact-form'>contact form</div>
+      <div className='sent-image'>Pop up sent image</div>
+      <div className='submit-button'>submit</div>
+      <div className='thank-you'>Thank you for stopping bye</div>
     </div>
   )
 }
