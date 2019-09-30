97b18e2cb4a133f721c543ff8fe94ed52f433dc5
Josh <jmart790@fiu.edu>
2019-09-30 09:06:42 -0400

    added icons for each project object

--- src/components/myProjects/projectData.js
+++ src/components/myProjects/projectData.js
@@ -3,36 +3,40 @@ export const projectData = [
     id: 0,
     image:
       'https://images.unsplash.com/photo-1559666126-84f389727b9a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1356&q=80',
-    title: 'Project 1',
+    title: 'Votelity',
     description:
       'skdfhjsdfjh sdkfh skdjf ksdf afjlkf fl jsf al laksjfl a slkfj jfalksjflk ajskfj lasjf laalksjfl jlasfk slk jalsf jljsaas',
-    github: 'github.com/project1'
+    github: 'github.com/project1',
+    icon: 'fas fa-flag-usa'
   },
   {
     id: 1,
     image:
       'https://images.unsplash.com/photo-1557389352-e721da78ad9f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
-    title: 'Project 2',
+    title: 'Tipsee',
     description:
       'skdfhjsdfjh sdkfh skdjf ksdf afjlkf fl jsf al laksjfl a slkfj jfalksjflk ajskfj lasjf laalksjfl jlasfk slk jalsf jljsaas',
-    github: 'github.com/project2'
+    github: 'github.com/project2',
+    icon: 'fas fa-wine-glass-alt'
   },
   {
     id: 2,
     image:
       'https://images.unsplash.com/photo-1553969420-fb915228af51?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1049&q=80',
-    title: 'Project 3',
+    title: 'Todo',
     description:
       'skdfhjsdfjh sdkfh skdjf ksdf afjlkf fl jsf al laksjfl a slkfj jfalksjflk ajskfj lasjf laalksjfl jlasfk slk jalsf jljsaas',
-    github: 'github.com/project3'
+    github: 'github.com/project3',
+    icon: 'fas fa-tasks'
   },
   {
     id: 3,
     image:
       'https://images.unsplash.com/photo-1550596334-7bb40a71b6bc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
-    title: 'Project 4',
+    title: 'My portfolio site',
     description:
       'skdfhjsdfjh sdkfh skdjf ksdf afjlkf fl jsf al laksjfl a slkfj jfalksjflk ajskfj lasjf laalksjfl jlasfk slk jalsf jljsaas',
-    github: 'github.com/project4'
+    github: 'github.com/project4',
+    icon: 'far fa-laugh-wink'
   }
 ]
