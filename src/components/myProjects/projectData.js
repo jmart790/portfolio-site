bcee016309023b99b400a69273fb05f99d9e6681
Josh <jmart790@fiu.edu>
2019-10-01 17:14:11 -0400

    added descriptions for projects

--- src/components/myProjects/projectData.js
+++ src/components/myProjects/projectData.js
@@ -4,8 +4,8 @@ export const projectData = [
     image: './images/votelity.png',
     title: 'Votelity',
     description:
-      'skdfhjsdfjh sdkfh skdjf ksdf afjlkf fl jsf al laksjfl a slkfj jfalksjflk ajskfj lasjf laalksjfl jlasfk slk jalsf jljsaas',
-    github: 'github.com/project1',
+      'A Ruby on Rails application which compares politicians based off their voting history. Vote Smart API was used to retrieve and store the data within postgreSQL. Ruby handled API and database calls, React.js rendered the data on the front end, and Heroku deployed the app and images separately. Created mockups/prototypes via Adobe XD',
+    github: 'https://github.com/wyncode/C34_anthony_gabriel_joshua_miguel',
     icon: 'fas fa-flag-usa'
   },
   {
@@ -13,8 +13,8 @@ export const projectData = [
     image: './images/tipsee.png',
     title: 'Tipsee',
     description:
-      'skdfhjsdfjh sdkfh skdjf ksdf afjlkf fl jsf al laksjfl a slkfj jfalksjflk ajskfj lasjf laalksjfl jlasfk slk jalsf jljsaas',
-    github: 'github.com/project2',
+      'A React application that retrieves multiple drink combinations after the user enters up to three ingredients. CocktailDB API was used to retrieve the data via Axios and rendered on the front end via React. Heroku was used to deploy.',
+    github: 'https://github.com/wyncode/joshua_matthew_michaela_nicole',
     icon: 'fas fa-wine-glass-alt'
   },
   {
@@ -22,8 +22,8 @@ export const projectData = [
     image: './images/todo.png',
     title: 'Todo',
     description:
-      'skdfhjsdfjh sdkfh skdjf ksdf afjlkf fl jsf al laksjfl a slkfj jfalksjflk ajskfj lasjf laalksjfl jlasfk slk jalsf jljsaas',
-    github: 'github.com/project3',
+      'A simple modern React To-do application where users can enter, mark complete, and delete tasks all while the app tracks the remaining incomplete tasks.',
+    github: 'https://github.com/jmart790/React-Todo-App',
     icon: 'fas fa-tasks'
   },
   {
@@ -31,8 +31,8 @@ export const projectData = [
     image: './images/accomplished.png',
     title: 'Portfolio',
     description:
-      'skdfhjsdfjh sdkfh skdjf ksdf afjlkf fl jsf al laksjfl a slkfj jfalksjflk ajskfj lasjf laalksjfl jlasfk slk jalsf jljsaas',
-    github: 'github.com/project4',
+      'A fun interactive React hooks application displaying my most recent projects, history, and contact form. Bitmoji was used for character content. Sendgrid API was used to forward emails.',
+    github: 'https://github.com/jmart790/portfolio-site',
     icon: 'far fa-laugh-wink'
   }
 ]
